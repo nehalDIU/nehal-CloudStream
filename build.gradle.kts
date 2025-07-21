048da29f40712e7e65232fde7d718bfaeb5dede6
nehalDIU <nehaldiu@gmail.com>
2025-07-21 19:53:28 +0600

    all working provider

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import com.android.build.gradle.BaseExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-import java.util.Properties
 
 buildscript {
     repositories {
@@ -32,18 +31,6 @@ fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extens
 
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
-// Function to get secrets from environment variables or provide defaults
-fun getSecret(key: String): String {
-    return System.getenv(key) ?: when (key) {
-        "MOVIEBOX_SECRET_KEY_DEFAULT" -> "default_key_1"
-        "MOVIEBOX_SECRET_KEY_ALT" -> "default_key_2"
-        "CASTLE_SUFFIX" -> "default_suffix"
-        "SIMKL_API" -> "default_simkl_api"
-        "MAL_API" -> "default_mal_api"
-        else -> "default_value"
-    }
-}
-
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
@@ -61,16 +48,6 @@ subprojects {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
-
-             // Inject secrets into BuildConfig
-            buildConfigField("String", "MOVIEBOX_SECRET_KEY_DEFAULT", "\"${getSecret("MOVIEBOX_SECRET_KEY_DEFAULT")}\"")
-            buildConfigField("String", "MOVIEBOX_SECRET_KEY_ALT", "\"${getSecret("MOVIEBOX_SECRET_KEY_ALT")}\"")
-            buildConfigField("String", "CASTLE_SUFFIX", "\"${getSecret("CASTLE_SUFFIX")}\"")
-            buildConfigField("String", "SIMKL_API", "\"${getSecret("SIMKL_API")}\"")
-            buildConfigField("String", "MAL_API", "\"${getSecret("MAL_API")}\"")
-            buildConfigField("String", "LIBRARY_PACKAGE_NAME", "\"com.nehal\"")
-
-            
         }
 
         compileOptions {
@@ -96,15 +73,6 @@ subprojects {
 
         // Stubs for all cloudstream classes
         cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.13")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-        implementation("com.squareup.okhttp3:okhttp:4.12.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
-        implementation("org.mozilla:rhino:1.8.0")
-        implementation("com.google.code.gson:gson:2.11.0")
 
         // These dependencies can include any of those which are added by the app,
         // but you don't need to include any of them if you don't need them.
