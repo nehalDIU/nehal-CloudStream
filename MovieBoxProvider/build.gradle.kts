268aaf19e77d12438a237e12e642c5c1301d0d87
Sheikh Shariar Nehal <nehaldiu@gmail.com>
2026-05-23 22:26:55 +0600

    fix

--- MovieBoxProvider/build.gradle.kts
+++ MovieBoxProvider/build.gradle.kts
@@ -7,6 +7,31 @@ android {
     }
 }
 
+android {
+    namespace = "com.cncverse"
+}
+
+android {
+    defaultConfig {
+        val smartlinkUrl = (project.findProperty("SMARTLINK_URL") as String?)
+            ?: System.getenv("SMARTLINK_URL")
+            ?: ""
+        val moneTag = (project.findProperty("MONE_TAG") as String?)
+            ?: System.getenv("MONE_TAG")
+            ?: ""
+
+        val escapedSmartlinkUrl = smartlinkUrl
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+        val escapedMoneTag = moneTag
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+
+        buildConfigField("String", "SMARTLINK_URL", "\"${escapedSmartlinkUrl}\"")
+        buildConfigField("String", "MONE_TAG", "\"${escapedMoneTag}\"")
+    }
+}
+
 cloudstream {
     language = "ta"
     // All of these properties are optional, you can safely remove them
