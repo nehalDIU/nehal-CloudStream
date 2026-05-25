7c51aa5bd743fb18683f7c8852731c024f59d33e
Sheikh Shariar Nehal <nehaldiu@gmail.com>
2026-05-26 02:32:51 +0600

    fix

--- CastleTvProvider/build.gradle.kts
+++ CastleTvProvider/build.gradle.kts
@@ -7,6 +7,38 @@ android {
     }
 }
 
+android {
+    namespace = "com.cncverse"
+}
+
+android {
+    defaultConfig {
+        val smartlinkUrl = (project.findProperty("SMARTLINK_URL") as String?)
+            ?: System.getenv("SMARTLINK_URL")
+            ?: ""
+        val speedlinkUrl = (project.findProperty("SPEEDLINK_URL") as String?)
+            ?: System.getenv("SPEEDLINK_URL")
+            ?: ""
+        val castleSuffix = (project.findProperty("CASTLE_SUFFIX") as String?)
+            ?: System.getenv("CASTLE_SUFFIX")
+            ?: ""
+
+        val escapedSmartlinkUrl = smartlinkUrl
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+        val escapedSpeedlinkUrl = speedlinkUrl
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+        val escapedCastleSuffix = castleSuffix
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")
+
+        buildConfigField("String", "SMARTLINK_URL", "\"${escapedSmartlinkUrl}\"")
+        buildConfigField("String", "SPEEDLINK_URL", "\"${escapedSpeedlinkUrl}\"")
+        buildConfigField("String", "CASTLE_SUFFIX", "\"${escapedCastleSuffix}\"")
+    }
+}
+
 cloudstream {
     language = "ta"
     // All of these properties are optional, you can safely remove them
